b44e6b02abc78de13bbe3bf6f9fa20073cfa450d
Minos Davi <minosdavilima@gmail.com>
2020-03-28 23:37:54 -0300

    release1.0.0

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import "package:flutter/material.dart";
+
+void main(){
+
+  runApp(
+      Center(
+        child: Text(
+            "hello flutter",
+            textDirection: TextDirection.ltr
+        ),
+
+      )
+  );
+}
